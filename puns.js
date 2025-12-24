8ac25dbc570b6b235f130e2149808dd058265288
jeremiahmcelwee <161647059+jeremiahmcelwee@users.noreply.github.com>
2025-12-24 10:39:42 -0800

    added pun

--- puns.js
+++ puns.js
@@ -3,7 +3,8 @@ const puns = [
     "What did one volcano say to the other? I lava you <3",
     "Do not take this website for granite",
     "My friend Phil swallowed a flashlight. Now he's a phyllite.",
-    "If you're fishing for a long-nosed fish, be sure to bring your garnet!"
+    "If you're fishing for a long-nosed fish, be sure to bring your garnet!",
+    "Geology rocks"
 ];
 
 // Setup a list of random integers corresponding to each pun
