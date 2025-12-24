42bcf7990b4227f50bc81768e4b70a509dacc90b
jeremiahmcelwee <161647059+jeremiahmcelwee@users.noreply.github.com>
2025-12-23 23:15:02 -0800

    Create puns.js
    Added javascript for picking a random pun. Script is referenced in html script.

---
+++ puns.js
@@ -0,0 +1,18 @@
+// List of puns
+const puns = [
+    "What did one volcano say to the other? I lava you <3",
+    "Do not take this website for granite",
+    "My friend Phil swallowed a flashlight. Now he's a phyllite.",
+    "If you're fishing for a long-nosed fish, be sure to bring your garnet!"
+];
+
+// Setup a list of random integers corresponding to each pun
+const randomIndex = Math.floor(Math.random() * puns.length);
+
+// ID for html script
+const punElement = document.getElementById("refresh-pun");
+if (punElement) {
+    punElement.innerText = puns[randomIndex]; // Pick a random pun
+} else {
+    console.error("Could not find element with id 'refresh-pun'");
+}
