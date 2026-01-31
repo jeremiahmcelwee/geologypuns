8ef3df67c7042ecb366948a6484dfd9e022185c9
jeremiahmcelwee <161647059+jeremiahmcelwee@users.noreply.github.com>
2026-01-30 19:31:59 -0600

    Update puns.js
    Added puns

--- puns.js
+++ puns.js
@@ -5,6 +5,14 @@ const puns = [
     "My friend Phil swallowed a flashlight. Now he's a phyllite.",
     "If you're fishing for a long-nosed fish, be sure to bring your garnet!",
     "Geology rocks"
+    "Why wasn't the geologist hungry? Because they lost their apatite"
+    "Basalt is so hipster. It was magma before it was cool."
+    "Why did the fold get in trouble. It was caught rolling a joint."
+    "This joke is a clastic"
+    "May the quartz be with you"
+    "Please be patient with geologists. They all have their faults."
+    "Tectonic plates can't maintain relationships with each other. There's just too much friction"
+    "I have to say, this website got off to a rocky start."
 ];
 
 // Setup a list of random integers corresponding to each pun
